5c65f37296b01cdd989140b335da4f1b82615287
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-04-30 13:03:10 +0200

    gairebé😭

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 20:10:03 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/04/29 21:07:19 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/04/30 13:01:26 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,33 @@
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	index;
+	unsigned int	src_len;
 	unsigned int	dest_len;
 
 	index = 0;
+	src_len = 0;
 	dest_len = 0;
-	printf("---\nd:%s, i:%s, s:%d\n", dest, src, size);
-	while (*dest != '\0')
+	while (dest[dest_len] != '\0')
 	{
-		dest++;
 		dest_len++;
 	}
-	while (src[index] != '\0' || index + dest_len <= size)
+	while (src[src_len] != '\0')
 	{
-		printf("\nd:%c, i:%c, s:%d\n", dest[index], src[index], index);
+		src_len++;
+	}
+	while (src[index] != '\0' || index < size + src_len)
+	{
+		if (index + dest_len + 1 < size )
+		{
+			(&dest[dest_len])[index] = src[index];
+		}
+		else if (index + dest_len + 1 == size && size != 0)
+		{
+			(&dest[dest_len])[index] = '\0';
+		}
 		if (src[index] == '\0')
 		{
-			dest[index] = src[index];
 			return (index);
-		}
-		else if (index + dest_len == size && size != 0)
-		{
-			dest[index] = '\0';
-			return (index);
-		}
-		if (index + dest_len < size)
-		{
-			dest[index] = src[index];
 		}
 		index++;
 	}
