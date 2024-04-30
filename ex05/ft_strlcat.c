8ffbf9ab80a67704b905d3f6e3e3e587093a25c2
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-04-30 15:09:10 +0200

    lo de les torres bessones semblarà poc al costat del que faré jo

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 20:10:03 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/04/30 13:01:26 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/04/30 15:05:58 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,27 @@
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	index;
-	unsigned int	src_len;
 	unsigned int	dest_len;
 
 	index = 0;
-	src_len = 0;
 	dest_len = 0;
-	while (dest[dest_len] != '\0')
+	while (dest[dest_len] != '\0' && dest_len < size)
 	{
 		dest_len++;
 	}
-	while (src[src_len] != '\0')
+	while (src[index] != '\0' || index < size + dest_len)
 	{
-		src_len++;
-	}
-	while (src[index] != '\0' || index < size + src_len)
-	{
-		if (index + dest_len + 1 < size )
+		if (index + dest_len + 1 < size)
 		{
 			(&dest[dest_len])[index] = src[index];
 		}
-		else if (index + dest_len + 1 == size && size != 0)
+		if (index + dest_len + 1 == size && size != 0)
 		{
 			(&dest[dest_len])[index] = '\0';
 		}
 		if (src[index] == '\0')
-		{
-			return (index);
-		}
+			break ;
 		index++;
 	}
-	return (index);
+	return (index + dest_len);
 }
