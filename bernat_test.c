9b7268b99222c9a55510eedb5cb19dce7a5a1db4
Bernat Canals I Guardia <bcanals-@cbr4s1.42barcelona.com>
2024-05-14 18:58:26 +0200

    provant provant 123

---
+++ bernat_test.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bernat_test.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcanals- <bcanals-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 11:11:10 by bcanals-          #+#    #+#             */
+/*   Updated: 2024/05/14 16:32:30 by bcanals-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datatypes.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+t_b_props	*make_map(int x, int y)
+{
+	t_b_props	*map;
+
+	map = malloc(sizeof(t_b_props));
+	map->size_x = x;
+	map->size_y = y;
+	return (map);
+}
+
+t_cell	**make_cells(int x, int y)
+{
+	t_cell	**fake_map;
+	int		i;
+	int		j;
+
+	i = 0;
+	fake_map = malloc(x * sizeof(t_cell *));
+	while (i < x)
+	{
+		j = 0;
+		fake_map[i] = malloc(y * sizeof(t_cell));
+		while (j < y)
+		{
+			fake_map[i][j].x = j;
+			fake_map[i][j].y = i;
+			fake_map[i][j].content = empty;
+			j++;
+		}
+		i++;
+	}
+	return (fake_map);
+}
+
+void	put_obstacle(t_cell **map, int x, int y)
+{
+	map[x][y].content = obstacle;
+}
+
+void	print_map(t_b_props *map_props, t_cell **map)
+{
+	int	i;
+	int	j;
+
+	i = map_props->size_y - 1;
+	while (i >= 0)
+	{
+		j = 0;
+		while (j < map_props->size_x)
+		{
+			printf("%i ", map[j][i].content);
+			j++;
+		}
+		printf("\n");
+		i--;
+	}
+}
+
+t_c_cont	get_cell_value(t_cell **map, int x, int y)
+{
+	return (map[x][y].content);
+}
--- main.c
+++ main.c
@@ -6,39 +6,34 @@
 /*   By: bcanals- <bcanals-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:02:26 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/05/14 10:19:53 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/05/14 16:35:27 by bcanals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
+#include "datatypes.h"
+#include "alg.h"
 #include <stdio.h>
-#include <unistd.h>
-#include "BSQ.h"
+#include <stdlib.h>
 
-int main (int argc, char **argv)
+int	min(int a, int b)
 {
-	int fd1;
-	int fd1_r;
-	struct info_line info;
-	
-	fd1 = open(argv[1], O_RDONLY);
-	printf("fd1 = %d\n", fd1);
-	fd1_r = read(fd1, &info.lines, 1);
-	ft_putstr("flag 1\n");
-	fd1_r  = read(fd1, &info.empty, 1);
-	ft_putstr("flag 2\n");
-	fd1_r = read(fd1, &info.obstacle, 1);
-	ft_putstr("flag 3\n");
-	fd1_r = read(fd1, &info.full, 1);
-	ft_putstr("flag 4\n");
-	fd1_r  = read(fd1, &info.new_line, 1);
-	ft_putstr("flag 5\n");
-	printf("read:\n\tlines = %c\n\tempty = %c\n\tobstc = %c\n\tfull  = %c",
-			info.lines, info.empty, info.obstacle, info.full);
-	if (info.new_line == '\n')
-		ft_putstr("\tEOL correct!");
-	else
-		ft_putstr("\tEOL NOT correct :(");
-	close(fd1);
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+int	main(int argc, char **argv)
+{
+	t_b_props	*map_props;
+	t_cell		**map;
+	t_cell		***arrays;
+	t_result	*answer;
+
 	argc++;
+	map_props = make_map(atoi(argv[1]), atoi(argv[2]));
+	map = make_cells(map_props->size_x, map_props->size_y);
+	put_obstacle(map, 1, 3);
+	print_map(map_props, map);
+	arrays = set_cell_array(map_props->size_x, map_props->size_y);
+	answer = get_result(map_props, map, arrays);
 }
