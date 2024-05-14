80d8a9978425529d4bee5589ea517229719792ee
Bernat Canals I Guardia <bcanals-@cbr4s1.42barcelona.com>
2024-05-14 10:24:37 +0200

    new tests

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 NAME = BSQ
 
 MY_SOURCES = ft_putchar.c \
+			 ft_putstr.c \
 			 main.c
+
 MY_OBJECTS = $(MY_SOURCES:.c=.o)
 
 CFLAGS = -Wall -Wextra -Werror
--- main.c
+++ main.c
@@ -6,14 +6,39 @@
 /*   By: bcanals- <bcanals-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:02:26 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/05/14 09:04:01 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/05/14 10:19:53 by bcanals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "BSQ.h"
 
 int main (int argc, char **argv)
 {
+	int fd1;
+	int fd1_r;
+	struct info_line info;
+	
+	fd1 = open(argv[1], O_RDONLY);
+	printf("fd1 = %d\n", fd1);
+	fd1_r = read(fd1, &info.lines, 1);
+	ft_putstr("flag 1\n");
+	fd1_r  = read(fd1, &info.empty, 1);
+	ft_putstr("flag 2\n");
+	fd1_r = read(fd1, &info.obstacle, 1);
+	ft_putstr("flag 3\n");
+	fd1_r = read(fd1, &info.full, 1);
+	ft_putstr("flag 4\n");
+	fd1_r  = read(fd1, &info.new_line, 1);
+	ft_putstr("flag 5\n");
+	printf("read:\n\tlines = %c\n\tempty = %c\n\tobstc = %c\n\tfull  = %c",
+			info.lines, info.empty, info.obstacle, info.full);
+	if (info.new_line == '\n')
+		ft_putstr("\tEOL correct!");
+	else
+		ft_putstr("\tEOL NOT correct :(");
+	close(fd1);
 	argc++;
-	ft_putchar(argv[1][0]);
 }
