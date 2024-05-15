23d7b433141d87f1325cee46db260abd064b333d
Bernat Canals I Guardia <bcanals-@cbr4s1.42barcelona.com>
2024-05-15 10:40:10 +0200

    testing pointers v2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bcanals- <bcanals-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:02:26 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/05/15 09:25:45 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:39:27 by bcanals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	main(int argc, char **argv)
+int	main()//int argc, char **argv)
 {
 	t_b_props	*map_props;
 	t_cell		**map;
@@ -25,8 +25,9 @@ int	main(int argc, char **argv)
 	//int lol1 = 0;
 	//int lol2 = 0;
 
-	argc++;
-	map_props = make_map(atoi(argv[1]), atoi(argv[2]));
+	//argc++;
+	//map_props = make_map(atoi(argv[1]), atoi(argv[2]));
+	map_props = make_map(5, 5);
 	map = make_cells(map_props->size_x, map_props->size_y);
 	/*while (i < (atoi(argv[1]) * atoi(argv[2])) / 3)
 	{
@@ -54,7 +55,7 @@ int	main(int argc, char **argv)
 	put_obstacle(map, 1, 0);
 	put_obstacle(map, 3, 2);
 	*/
-	put_obstacle(map, atoi(argv[3]), atoi(argv[4]));
+	//put_obstacle(map, atoi(argv[3]), atoi(argv[4]));
 	//put_obstacle(map, atoi(argv[5]), atoi(argv[6]));
 	print_map(map_props, map);
 	arrays = set_cell_array(map_props->size_x, map_props->size_y);
@@ -62,3 +63,4 @@ int	main(int argc, char **argv)
 	answer = get_result(map_props, map, arrays);
 	print_arrays(arrays, map_props);
 }
+
